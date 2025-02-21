66622006ed882ea4486be8396e5d9142589608ad
Brian Dahlem <dahlem.brian@cusd80.com>
2025-02-21 07:03:16 -0700

    updated instructions to install extensions semi-automatically

--- package.ps1
+++ package.ps1
@@ -7,11 +7,13 @@ if (-not (Test-Path $destinationDir)) {
 
 # Compress each subdirectory into a ZIP file
 Get-ChildItem -Directory | ForEach-Object {
-    if ($_.Name -eq $destinationDir -or $_.Name -eq "__pycache__") {
+    if ($_.Name -eq $destinationDir -or $_.Name -eq "__pycache__" -or $_.Name -eq ".git" -or $_.Name -eq ".vscode") {
         return
     }
     $folderName = $_.Name
     $zipFileName = "$destinationDir\$folderName.zip"
     Compress-Archive -Path (Join-Path $_.FullName "*") -DestinationPath $zipFileName -Force -CompressionLevel Optimal
+    Compress-Archive -Path ".\.vscode" -DestinationPath $zipFileName -Update -CompressionLevel Optimal
     Write-Host "Created: $zipFileName"
+
 }
